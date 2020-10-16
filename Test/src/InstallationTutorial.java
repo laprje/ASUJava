eb2bdd6c573cf0c35bea1ef3ca09a190f677089d
L J <laprje@gmail.com>
2020-10-15 18:33:20 -0600

    initial commit with Lab1

---
+++ Test/src/InstallationTutorial.java
@@ -0,0 +1,5 @@
+public class InstallationTutorial {
+    public static void main (String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ test2/src/testing.java
@@ -0,0 +1,8 @@
+public class testing {
+    public static void main (String[] args) {
+//        System.out.printf("%8,2f", 54321.0);
+//        System.out.printf("%8.00f", 54321.0);
+        System.out.printf("%8.2f", 54321.0);
+//        System.out.printf(",8.2f", 54321.0);
+    }
+}
