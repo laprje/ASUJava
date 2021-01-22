1dc62ef1e8f5b698462eca78d2ec4f40e004bb30
L J <laprje@gmail.com>
2021-01-21 19:29:17 -0700

    finished project

--- CSE205-P1/src/Main.java
+++ CSE205-P1/src/Main.java
@@ -13,23 +13,36 @@
  * AUTHOR 2: Lansing Jenkins, ljenki14, ljenki14@asu.edu
  * AUTHOR 3: Gregory Ingraham, gingrah1, gingrah1@asu.edu ********************************************************************************************************/
 
-import java.net.*;
+import java.net.URL;
 import java.util.*;
 import java.io.*;
-
 
 
 
 public class Main {
     final int RUNS_UP = 1, RUNS_DN = -1;
     public static void main(String[] pArgs) {
-       new Main().run();
+        new Main().run();
     }
 
     /**
      * Used to run a combination of our other methods.
      */
     private void run() {
+        ArrayList<Integer> list = new ArrayList<>();
+        list = readInputFile("p1-in.txt");
+
+
+
+        ArrayList<Integer> listRunsUpCount = new ArrayList<>();
+        ArrayList<Integer> listRunsDnCount = new ArrayList<>();
+        listRunsUpCount = findRuns(list, RUNS_UP);
+        listRunsDnCount = findRuns(list, RUNS_DN);
+
+        ArrayList<Integer> listRunsCount = new ArrayList<>();
+        listRunsCount = mergeLists(listRunsUpCount, listRunsDnCount);
+
+        writeOutputFile("p1-runs.txt", listRunsCount);
 
     }
 
@@ -37,19 +50,20 @@ public class Main {
      * Searches through the ArrayList passed as a parameter for runs, up or down depending on the pDir param.
      */
     public ArrayList<Integer> findRuns(ArrayList<Integer> pList, int pDir) {
-        //here is some unfinished logic for finding runs
         ArrayList<Integer> listRunsCount = arrayListCreate(pList.size(),0);
         int k = 0;
         int i = 0;
         while(i<pList.size()-1){
-                if (pDir > 0 && pList.get(i) <= pList.get(i+1)) {
-                    k++;
-                } else if(pDir < 0 && pList.get(i) >= pList.get(i+1)){
-                    k++;
-                } else if (k != 0){
-                    listRunsCount.set(k,listRunsCount.get(k)+1);
-                }
+            if (pDir > 0 && pList.get(i) <= pList.get(i+1)) {
+                k++;
+            } else if(pDir < 0 && pList.get(i) >= pList.get(i+1)){
+                k++;
+            } else if (k != 0){
+                listRunsCount.set(k,listRunsCount.get(k)+1);
+                k = 0;
             }
+            i++;
+        }
         if(k != 0){
             listRunsCount.set(k,listRunsCount.get(k)+1);
         }
@@ -58,15 +72,16 @@ public class Main {
 
 
     /**
-     * NEED MERGELISTS METHOD HERE
+     * Returns ArrayList of Integers which is the sum of elements of listRunsUpCount and listRunsDownCount.
      */
-    /**
-     * NEED MERGELISTS METHOD HERE
-     */
-    /**
-     * NEED MERGELISTS METHOD HERE
-     */
+    private  ArrayList<Integer> mergeLists(ArrayList<Integer> pListRunsUpCount, ArrayList<Integer> pListRunsDnCount) {
+        ArrayList<Integer> listRunsCount = arrayListCreate(pListRunsUpCount.size(), 0);
 
+        for(int i = 0; i<pListRunsUpCount.size(); i++) {
+            listRunsCount.set(i, pListRunsUpCount.get(i) + pListRunsDnCount.get(i));
+        }
+        return listRunsCount;
+    }
 
     /**
      * Creates new ArrayList.
@@ -89,26 +104,31 @@ public class Main {
             for (int i = 0; i < pListRuns.size(); i++) {
                 sum += pListRuns.get(i);
             }
-            out.write("runs_total: " + sum);
+            out.write("runs_total: " + sum );
             for(int k=1; k<pListRuns.size(); k++){
-                out.write("runs_" + k + pListRuns.get(k));
+                out.write("\nruns_" + k + ": " + pListRuns.get(k));
+                out.flush();
             }
         }
         catch(FileNotFoundException e) {
-            System.out.println("Oops, could not open 'p1-runs.txt' for writing. The program is ending.");
+            System.out.println("Oops, could not open " + pFilename + " for writing. The program is ending.");
             e.printStackTrace();
         }
         catch (IOException e) {
-            System.out.println("Oops, could not open 'p1-runs.txt' for writing. The program is ending.");
+            System.out.println("Oops, could not open " + pFilename + " for writing. The program is ending.");
             e.printStackTrace();
+        }
+        finally {
+            System.out.print("File " + pFilename + " successfully written.");
         }
     }
 
     /**
      * Reads the input file, scans it, and puts each integer into the ArrayList 'list'.
      */
-    public ArrayList<Integer> readInputFile(String pFilename, ArrayList<Integer> pListRuns){
+    public ArrayList<Integer> readInputFile(String pFilename){
         ArrayList<Integer> list = new ArrayList<>();
+//        File f = new File(pFilename);
         try {
             URL path = Main.class.getResource("p1-in.txt");
             File f = new File(path.getFile());
@@ -119,12 +139,11 @@ public class Main {
             scan.close();
         }
         catch(FileNotFoundException e){
-            System.out.println("Oops, could not open 'p1-in.txt' for reading. The program is ending.");
+            System.out.println("Oops, could not open " + pFilename + " for reading. The program is ending.");
             e.printStackTrace();
             System.exit(-100);
         }
         finally {
-            System.out.print(list);
         }
         return list;
     }
